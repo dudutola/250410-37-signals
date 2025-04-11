ac73caa099f0e983154535ee54c6356a6b86ca3f
Dulcelene Machado <dudutola@gmail.com>
2025-04-11 17:00:10 +0100

    Implemented handling of various date formats Utilized Date::MONTHNAMES for easier month lookup Fixed invalid date errors by checking for valid dates before applying time parsing Refined am/pm logic to only apply conversion when period is present

--- parse_natural_dates.rb
+++ parse_natural_dates.rb
@@ -9,6 +9,7 @@ def parse_natural_dates(expression)
   target_date = nil
   time_string = "12:00"
 
+  # Match "next <day of the week>"
   if expression.match(/(next) (sunday|monday|tuesday|wednesday|thursday|friday|saturday)/)
     next_match = $1
     day_of_the_week = $2
@@ -21,31 +22,72 @@ def parse_natural_dates(expression)
     end
   end
 
+  # Match the time of day
   if expression.match(/(morning|noon|afternoon|evening)/)
     time_of_the_day = $1
     time_index = times.find_index { |time| time_of_the_day.downcase == time }
     time_string = times_values[time_index] if time_index
   end
 
+  # Match "today" or "tomorrow"
   if expression.match?(/\b(today|this day)\b/)
     target_date = today
   elsif expression.match?(/\b(tomorrow|next day)\b/)
     target_date = today + 1
   end
 
+  # Match "am/pm"
   if expression.match(/(\d{1,2})(?::(\d{2}))?(am|pm)?/)
     hour = $1.to_i
     minutes = $2 ? $2.to_i : 0
     period = $3
 
-    if period == "pm" && hour << 12
-      hour += 12
-    elsif period == "am" && hour == 12
-      hour = 0
-      # binding.pry
+    if period
+      if period == "pm" && hour << 12
+        hour += 12
+      elsif period == "am" && hour == 12
+        hour = 0
+      end
+      time_string = "%02d:%02d" % [hour, minutes]
     end
+  end
 
-    time_string = "%02d:%02d" % [hour, minutes]
+  # Match "22nd of June" or "January 5"
+  if expression.match(/\b(\d{1,2})(st|nd|rd|th)? of (\w+)\b/)
+    day = $1.to_i
+    month = Date::MONTHNAMES.find_index($3.downcase.capitalize)
+    target_date = Date.new(today.year, month, day) if month
+  elsif expression.match(/\b(\w+) (\d{1,2})(st|nd|rd|th)?\b/)
+    month = Date::MONTHNAMES.find_index($1.downcase.capitalize)
+    day = $2.to_i
+    target_date = Date.new(today.year, month, day) if month
+  end
+
+  # Match "DD/MM/YYYY" or "DD/MM/YY"
+  if expression.match(/\b(\d{2})\/(\d{2})\/(\d{2}|\d{4})\b/)
+    day = $1.to_i
+    month = $2.to_i
+    year = $3.to_i
+    year += 2000 if year < 100
+    target_date = Date.new(year, month, day)
+  end
+
+
+  # if expression.match(/\b(\w+)\s(\d{1,2})(st|nd|rd|th)?\b/)
+  #   day_of_week = $1.to_i
+  #   day_of_month = $2.to_i
+  # elsif expression.match(/\b(\d{2})\/(\d{2})\/(\d{2}|\d{4})\b/)
+  #   exp = expression.match(/\b(\d{2})\/(\d{2})\/(\d{2}|\d{4})\b/)
+  #   binding.pry
+  #   day = $1.to_i
+  #   month = $2.to_i
+  #   year = $3.to_i
+  # end
+
+  # binding.pry
+
+  if target_date.nil?
+    raise ArgumentError, "Invalid date in expression: #{expression}"
   end
 
   datetime_string = "#{target_date} #{time_string}"
@@ -55,10 +97,14 @@ end
 # puts parse_natural_dates("Leadership lunch next friday at noon")
 # puts parse_natural_dates("Leadership lunch today at noon")
 # puts parse_natural_dates("Leadership lunch tomorrow")
-puts parse_natural_dates("Leadership lunch this day")
-puts parse_natural_dates("Leadership lunch next day")
+# puts parse_natural_dates("Leadership lunch this day")
+# puts parse_natural_dates("Leadership lunch next day")
 # puts parse_natural_dates("Meetup tomorrow at 5pm")
 # puts parse_natural_dates("Meetup tomorrow at 5:00")
-puts parse_natural_dates("03/01/2012 07:25:09.234567")
+# puts parse_natural_dates("03/01/2012 07:25:09.234567")
+# puts parse_natural_dates("22nd of june at 8am")
+# puts parse_natural_dates("January 5 at 7pm")
+puts parse_natural_dates("25/04/25")
+puts parse_natural_dates("25/04/2025")
 # puts parse_natural_dates("24h")
 # puts parse_natural_dates("Meetup todayhour at 8am")
