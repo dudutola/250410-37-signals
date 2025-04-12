77562695fa6b0c352f7a4b0eaaa3a2ef819f1422
Dulcelene Machado <dudutola@gmail.com>
2025-04-12 12:25:25 +0100

    Refactored data parsing logic Created an array of formats with regex and associated logic to handle the date

--- form_parser copy.js
+++ form_parser copy.js
@@ -1,10 +1,13 @@
+// get form + inputs
+// add event listener to input natural-text
+
+const formElement = document.getElementById("parsing-form");
 const inputNaturalTextElement = document.getElementById("natural-text");
 const inputStartDateElement = document.getElementById("start-date");
 const inputEndDateElement = document.getElementById("end-date");
 
 const today = new Date();
 const targetDate = new Date(today);
-
 // create arrays with days
 const weekdays = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 // create arrays with time of days
@@ -14,34 +17,46 @@ const timesValues = ["09:00", "12:00", "14:00", "18:00"];
 let timeString = "12:00";
 
 inputNaturalTextElement.addEventListener("keyup", (e) => {
+  console.log(e);
   e.preventDefault();
 
+  // get input value
   const textValue = inputNaturalTextElement.value.toLowerCase();
+
   let newDate;
+  // iterate through formats
+  // for (const format of formats) {
+  //   if something matches format.regex {
+  //    newDate = format.logic(textValue)
+  //   }
 
-  for (const format of formats) {
-    if (textValue.match(format.regex)) {
-      result = format.logic(textValue);
+  // }
 
-      // daysUntilTarget
-      if (result) {
-        if (format.regex === formats[0].regex) {
-          newDate = result;
-        }
-      }
-    };
+  const listOfMatchResult = textValue.match(/(next) (sunday|monday|tuesday|wednesday|thursday|friday|saturday)/);
+  // if the input includes next friday ....
+  if (listOfMatchResult) {
+    const nextMatch = listOfMatchResult[1];
+    const dayOfTheWeek = listOfMatchResult[2];
 
-    if (newDate) {
-      targetDate.setDate(today.getDate() + newDate);
+    const foundDay = weekdays.find(day => dayOfTheWeek.toLowerCase() === day.toLowerCase());
+
+    if (foundDay) {
+      const targetDayIndex = weekdays.indexOf(foundDay);
+      const currentDayIndex = today.getDay();
+      let daysUntilTarget = (targetDayIndex - currentDayIndex + 7) % 7
+      if (daysUntilTarget === 0) daysUntilTarget = 7
+
+      // const targetDate = new Date(today); // out
+      targetDate.setDate(today.getDate() + daysUntilTarget);
 
       const isoDate = targetDate.toISOString().split("T")[0];
-
+      console.log(isoDate)
       const fullDateTime = `${isoDate}T${timeString}`
       const fullsStartDateTime = `${today.toISOString().split("T")[0]}T${timeString}`
 
       inputStartDateElement.value = fullsStartDateTime;
       inputEndDateElement.value = fullDateTime;
-    }
+    };
   };
 
   const listOfTimeOfDay = textValue.match(/(morning|noon|afternoon|evening)/);
@@ -62,26 +77,13 @@ inputNaturalTextElement.addEventListener("keyup", (e) => {
 
 });
 
-const formats = [
-  {
-    regex: /(next)?\s(sunday|monday|tuesday|wednesday|thursday|friday|saturday)/,
-    logic: (textValue) => {
-      const listOfMatchResult = textValue.match(/(next)?\s(sunday|monday|tuesday|wednesday|thursday|friday|saturday)/);
-
-      if (listOfMatchResult) {
-        const dayOfTheWeek = listOfMatchResult[2];
-
-        const foundDay = weekdays.find(day => dayOfTheWeek.toLowerCase() === day.toLowerCase());
-
-        if (foundDay) {
-          const targetDayIndex = weekdays.indexOf(foundDay);
-          const currentDayIndex = today.getDay();
-          let daysUntilTarget = (targetDayIndex - currentDayIndex + 7) % 7
-          if (daysUntilTarget === 0) daysUntilTarget = 7
-
-          return daysUntilTarget;
-        };
-      };
-    },
-  }
-]
+// const formats = [
+//   {
+//     regex: /regex/,
+//     logic: (expression) => {
+//       logic...
+//       return newDate
+//     }
+//   },
+//   {}
+// ]
