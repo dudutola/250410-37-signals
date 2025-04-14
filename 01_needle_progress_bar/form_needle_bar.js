dff1bd960d66e5adf4f5cb316d1bffcb9c39b534
Dulcelene Machado <dudutola@gmail.com>
2025-04-14 10:42:40 +0100

    Added a marker on the range bar for the previous value after thumb is released and styled it

--- 01_needle_progress_bar/form_needle_bar.js
+++ 01_needle_progress_bar/form_needle_bar.js
@@ -1,20 +1,54 @@
 // retrieve all the important stuff
 const rangeInput = document.getElementById("range-bar");
 const colorButtons = document.querySelectorAll(".color-circle");
+const prevIndicator = document.getElementById("prev-indicator");
 
 // set default color
 let currentColor = "green";
+let defaultColor = "lightgray";
+let previousValue = null;
+
+// rangeInput.addEventListener("input", () => {
+//   const currentValue = rangeInput.value;
+
+//   if (previousValue !== null) {
+//     const marker = document.querySelector(".previous-marker");
+//     const percent = (previousValue / rangeInput.max) * 100;
+//     marker.style.left = `${percent}%`;
+//   }
+
+//   previousValue = currentValue;
+// });
 
 // gradient background based on range value
 function updateRangeColor() {
-  const value = rangeInput.value;
-  const max = rangeInput.max || 94;
-  const percentage = (value / max) * 94;
+  // const value = rangeInput.value;
+  const value = parseInt(rangeInput.value);
+  const max = rangeInput.max || 100;
+  const percentage = (value / max) * 100;
 
-  rangeInput.style.background = `linear-gradient(to right, ${currentColor} ${percentage}%, lightgray ${percentage}%)`;
+  if (currentColor == "green") {
+    rangeInput.style.background = `linear-gradient(to right, ${currentColor} ${percentage}%, #f2f1f1 ${percentage}%)`;
+  } else {
+    rangeInput.style.background = `linear-gradient(to right, ${defaultColor} ${percentage}%, #f2f1f1 ${percentage}%)`;
+  }
 
   // set the thumb color
-  rangeInput.style.setProperty('--thumb-color', currentColor);
+  rangeInput.style.setProperty("--thumb-color", currentColor);
+}
+
+function updatePreviousIndicator() {
+  if (previousValue !== null) {
+    const max = rangeInput.max || 100;
+    const wrapperWidth = rangeInput.offsetWidth;
+    const prevPercentage = (previousValue / max) * 100;
+    const leftOffset = (prevPercentage / max) * wrapperWidth;
+
+    prevIndicator.style.left = `${leftOffset}px`;
+    prevIndicator.style.display = "block";
+  }
+  // save prevAlue with value
+  previousValue = parseInt(rangeInput.value);
 }
 
 // click to change range color
@@ -26,6 +60,7 @@ colorButtons.forEach(button => {
 });
 
 rangeInput.addEventListener("input", updateRangeColor);
+rangeInput.addEventListener("change", updatePreviousIndicator);
 
 updateRangeColor();
 
